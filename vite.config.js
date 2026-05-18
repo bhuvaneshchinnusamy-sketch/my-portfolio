a6dde67ed8241e84583d158b538f86e703101e39
bhuvaneshchinnusamy-sketch <bhuvaneshchinnusamy@gmail.com>
2026-05-18 07:15:59 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/my-portfolio/', // Set the base path to match the GitHub repository name
-  plugins: [react()]
-});
